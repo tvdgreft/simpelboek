7d7f6e3e689d5cb04a2d4de789c0632034d47f23
tvdgreft <tvdgreft@pranamas.nl>
2021-10-28 10:47:43 +0200

    fouten hersteld

--- simpelboek.php
+++ simpelboek.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 Plugin Name: simpelboek
 Plugin URI:  https://github.com/pranamas/wp/hello-world
 Description: raamwerk voor wordpress plugin
-Version:     1.0
+Version:     1.1
 Author:      Theo van der Greft
 Author URI:  http://www.pranamas.nl
 */
